f6bf6123d0a5ec0086c68676ec19c9adfe144545
robert-pierce <rpierce3@gmail.com>
2018-04-17 10:02:49 -0400

    added emacs.d

---
+++ emacs/.emacs.d/.emacs.d/elpa/flycheck-elm-20160717.257/flycheck-elm-autoloads.el
@@ -0,0 +1,27 @@
+;;; flycheck-elm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "flycheck-elm" "flycheck-elm.el" (22849 17390
+;;;;;;  0 0))
+;;; Generated autoloads from flycheck-elm.el
+
+(autoload 'flycheck-elm-setup "flycheck-elm" "\
+Setup Flycheck elm.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flycheck-elm-pkg.el") (22849 17390 740096
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-elm-autoloads.el ends here
